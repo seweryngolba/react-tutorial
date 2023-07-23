cf0e06e1657ef3e5dbdaea76fe2b879c307b8e07
Seweryn Golba <sgoolb@gmail.com>
2023-07-23 21:08:53 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import "./App.css";
 import { useState } from "react";
+import Task from "./Task";
 
 function App() {
-  const [textColor, setTextColor] = useState("black");
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    // const newTodoList = [...todoList, newTask];
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    // const newTodoList = todoList.filter((task) => task !== taskName);
+
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
 
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setTextColor(textColor === "black" ? "red" : "black");
-        }}
-      >
-        Show/Hide
-      </button>
-      <h1 style={{ color: textColor }}>HI World</h1>
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
